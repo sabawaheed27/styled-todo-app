556553e9a183c7767140dd9ca4f9a418b42d16e3
Saba Waheed <saba_sh27@hotmail.com>
2025-05-13 14:32:27 +0200

    added planning

---
+++ src/app/components/Header.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import styled from "styled-components"
+
+const StyledHeader = styled.header`
+background-color: #333;
+color: white;
+padding: 24px;
+text-align: center;
+`;
+
+const StyledPageTitle = styled.h1`
+margin: 24px;
+font-size: 1.8rem;
+font-color: #eef1ef;
+text-align: center;
+
+`;
+
+export default function Header(){
+    return(
+        <StyledHeader>
+            <StyledPageTitle>Todo App</StyledPageTitle>
+        </StyledHeader>
+    )
+}
